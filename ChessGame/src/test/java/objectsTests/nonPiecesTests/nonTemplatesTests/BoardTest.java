901f8329453b6717f1f19accefa7bbc040724f5f
Miguel Garcia <miguel.garcia42@educa.madrid.org>
2024-04-16 19:34:27 +0200

    Imagenes png de la carpeta diagramas acrualizada para mejor comprensión

--- ChessGame/src/test/java/objectsTests/nonPiecesTests/nonTemplatesTests/BoardTest.java
+++ ChessGame/src/test/java/objectsTests/nonPiecesTests/nonTemplatesTests/BoardTest.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package piecesTests;
+package objectsTests.nonPiecesTests.nonTemplatesTests;
 
 /**
  *
- * @author migue
+ * @author dev
  */
-public class QueenTest {
+public class BoardTest {
     
 }
---
+++ ChessGame/src/test/java/objectsTests/nonPiecesTests/templatesTests/DiagonalsMovesTest.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objectsTests.nonPiecesTests.templatesTests;
+
+/**
+ *
+ * @author dev
+ */
+public class DiagonalsMovesTest {
+    
+}
